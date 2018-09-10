d82d7aaeb110c74788bb3066f7ac6f1ba233db9f
tima-akulich <tima.akulich@gmail.com>
2018-09-10 20:48:54 +0300

    Added hometasks and functions

---
+++ homework.py
@@ -0,0 +1,61 @@
+# 1 task
+number = int(input("Your number"))
+
+action = None
+user_action = None
+
+while not action:
+    user_action = input("Bytes(b)? or kByte(kb)")
+    if user_action == 'b':
+        action = 1024
+    elif user_action == 'kb':
+        action = 1.0 / 1024
+    else:
+        print("Try again")
+
+print(int(number * action))
+
+# 2 task
+number = int(input())
+
+_sum = 0
+_mul = 1
+
+while number > 0:
+    last_sym = number % 10
+    number //= 10
+    _sum += last_sym
+    _mul *= last_sym
+
+# 3 task
+start = input()
+end = input()
+step = input()
+
+for i in range(start, end, step):
+    print(-1.24 * i ** 2 + i)
+
+# 4 task
+
+value = input("Type number")
+
+for i in range(len(value) / 2):
+    if value[i] != value[-(i+1)]:
+        print('Not palindrome')
+        break
+else:
+    print('Palindrome')
+
+# 5 task
+a = [1, 2, -3]
+
+_sum = 0
+_count = 0
+for value in a:
+    if value > 0:
+        _sum += value
+        _count += 1
+print(_sum / (_count or 1))
+
+
+
---
+++ lesson3.py
@@ -0,0 +1,111 @@
+_list = [1, 2, 3]
+print(_list)
+del _list[0]
+print(_list)
+del _list[:]
+print(_list)
+# del a
+# print(a)
+
+_dict = {'key1': 1, 'key2': 2}
+del _dict['key1']
+print(_dict)
+
+# Function
+
+
+def foo1():
+    pass
+
+
+def foo_with_return():
+    return True
+
+
+def foo_with_params(param1=True, param2=True):
+    pass
+
+foo_with_params(False, False)
+
+foo_with_params(param2=False, param1=True)
+
+foo_with_params(False, param2=True)
+
+# DONT DO THIS
+#  foo_with_params(param2=True, False)
+
+
+def foo_args(*args):
+    print('FOO ARGS', args)
+
+foo_args(1, 2, 3, 4, 5)
+
+
+def foo_kwargs(**kwargs):
+    print('FOO KWARGS', kwargs)
+
+foo_kwargs(a=1, b=2)
+
+print('#' * 10)
+
+
+def foo_args_kwargs(*args, **kwargs):
+    print('FOO ARGS', args)
+    print('FOO KWARGS', kwargs['a'])
+
+foo_args_kwargs(1, 2, 3, 4, 5, a=1, b=2, c=3)
+
+
+(lambda a, b: a + b)(1, 2)
+foo = lambda a, b: a + b
+
+
+def foo_sum(a, b):
+    return a + b
+
+
+def foo_arr(_list, operation=None):
+    result = 0
+    for elem in _list:
+        result = operation(result, elem)
+    return result
+
+print(
+    foo_arr([1, 2, 3],
+    operation=lambda x, y: x + y)
+)
+print(
+    foo_arr([1, 2, 3],
+    operation=lambda x, y: x - y)
+)
+foo_arr([1,2,3], foo_sum)
+
+
+# TASK 22
+def foo1(x):
+    return x ** 2
+
+
+def foo2(x):
+    return 2 * x
+
+
+def foo3(x):
+    return 2 * x - 1
+
+
+def foo_result(x):
+    result = None
+    if -5 <= x <= 5:
+        result = foo1(x)
+    elif x > 5:
+        result = foo2(x)
+    elif x < -5:
+        result = foo3(x)
+
+    return result
+
+arr = []
+for i in range(-10, 10, 1):
+    arr.append(foo_result(i))
+
